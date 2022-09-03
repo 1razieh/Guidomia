28be335ff72bb564dc324f666fd6b2236b3d60e0
razieh <halimi.razieh@gmail.com>
2022-09-03 10:59:11 -0400

    Car view model

---
+++ Guidomia/ViewModel/CarViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  CarViewModel.swift
+//  Guidomia
+//
+//  Created by Razieh Halimi on 2022-08-31.
+//
+
+import Foundation
+
+class CarListViewModel {
+    
+    var ListOfCar = [CarViewModel]()
+    
+    init() {
+        populateData()
+    }
+    
+    func populateData() {
+        WebService().getData { cars in
+            self.ListOfCar = cars.map(CarViewModel.init)
+
+        }
+    }
+}
+
+struct CarViewModel {
+    
+    var car: Car
+    
+    var consList: [String] {
+        return car.consList
+    }
+    
+    var prosList: [String] {
+        return car.prosList
+    }
+    
+    var customerPrice: Double {
+        return car.customerPrice
+    }
+    
+    var makeAndModel: String {
+        return car.make + " " + car.model
+    }
+    
+    var make: String {
+        return car.make
+    }
+    
+    var rating: Int {
+        return car.rating
+    }
+    
+    var marketPrice: String {
+        let price = Int(customerPrice / 1000)
+        return  String(price) + "K"
+       
+    }
+    
+}
