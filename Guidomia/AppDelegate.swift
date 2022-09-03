2fdecb53b726a819f8005f974ac9535338af4396
razieh <halimi.razieh@gmail.com>
2022-09-03 12:47:13 -0400

    Update navigation bar color

--- Guidomia/AppDelegate.swift
+++ Guidomia/AppDelegate.swift
@@ -14,7 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UINavigationBar.appearance().backgroundColor = UIColor(hexString: "#FC6016")
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+
+        
+      
         return true
     }
 
