725e2627e0d8d2bf3b42852af500664120f58ce6
razieh <halimi.razieh@gmail.com>
2022-09-05 01:33:15 -0400

    Create file for constant

---
+++ Guidomia/Utils/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Guidomia
+//
+//  Created by Razieh Halimi on 2022-09-04.
+//
+
+import Foundation
+import UIKit
+
+let expandedRowHeight: CGFloat = 375.0
+let defaultRowHeight: CGFloat = 110.0
+let priceSign: String = "k"
+
+
