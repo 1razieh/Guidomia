a9d7d4d8ebe5a579b6fa5c8b4080270b51d26d11
razieh <halimi.razieh@gmail.com>
2022-09-05 01:35:18 -0400

    Add unit test

--- GuidomiaTests/GuidomiaTests.swift
+++ GuidomiaTests/GuidomiaTests.swift
@@ -9,28 +9,34 @@ import XCTest
 @testable import Guidomia
 
 class GuidomiaTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testGetData() {
+        if let fileLocation = Bundle.main.url(forResource: "data", withExtension: "json"){
+            do{
+                let content = try Data(contentsOf: fileLocation)
+                guard parseJson(content) != nil else {
+                    XCTAssertNil(parseJson(content), "no data")
+                    return
+                }
+                if let cars = self.parseJson(content) {
+                    XCTAssertTrue(cars.count > 0 )
+                }
+            }
+            catch {
+                XCTAssertThrowsError(NetWorkError.decodingError.localizedDescription)
+            }
         }
     }
-
+    
+    func parseJson(_ data: Data) -> [Car]? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode([Car].self, from: data)
+            return decodedData
+            
+        } catch {
+            print(NetWorkError.decodingError.localizedDescription)
+            return nil
+        }
+    }
 }
